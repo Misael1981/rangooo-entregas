01f6c1812e0525c47299de67b723cf7fc89b7e1b
Misael <misaelborges1981@gmail.com>
2026-03-18 11:37:12 -0300

    Transformando aplicação em PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/providers/auth-provider";
@@ -16,10 +16,41 @@ const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
 });
 
+export const viewport: Viewport = {
+  themeColor: "#0f1d38",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export const metadata: Metadata = {
   title: "Rangooo Entregas",
   description:
     "Seu delivery mais rápido, seu negócio mais organizado. O Rangooo cuida de tudo.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    shortcut: ["/favicon-96x96.png"],
+    other: [{ rel: "mask-icon", url: "/logos.svg", color: "#E9ECF2" }],
+  },
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Rangooo Entregas",
+    // startupImage: [ ... ] // Para aquela splash screen de abertura depois
+  },
+
+  formatDetection: {
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
